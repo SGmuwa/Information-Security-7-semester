fb060fd75305813ddf1df7b02e308da7821096a0
SG_Muwa <motherlode.muwa@gmail.com>
2019-09-23 23:49:36 +0300

    Отлаживаю тест

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -3,6 +3,7 @@ using Caesar_s_code;
 using System.IO;
 using System;
 using System.Text;
+using System.Linq;
 
 namespace UnitTestProject1
 {
@@ -28,25 +29,47 @@ namespace UnitTestProject1
             }
         }
 
+        private MemoryStream CreateStream(string data, Encoding encoding)
+        {
+            MemoryStream output = new MemoryStream();
+            StreamWriter writer = new StreamWriter(output, encoding);
+            writer.Write(data);
+            writer.Flush();
+            output.Position = 0;
+            return output;
+        }
+
         [DataTestMethod]
-        [DataRow("рсс", "стт", Enc.ASCII)]
-        public void TestMethod1(string input, string expect, Enc enc)
+        [DataRow("рсс", new byte[] { 240, 188, 192, 209, 177, 209, 178, 209, 178 }, Enc.UTF8, (byte)1)]
+        public void TestMethod1(string input, byte[] expect, Enc enc, byte key)
         {
             Encoding encoding = GetEncoding(enc);
-            MemoryStream ToEncrypt = new MemoryStream();
-            new StreamWriter(ToEncrypt, encoding).WriteAsync(input);
-            MemoryStream Expect = new MemoryStream();
-            new StreamWriter(Expect, encoding).WriteAsync(expect);
-            MemoryStream OutputEncrypt = new MemoryStream();
-            Encryption.Encrypt(OutputEncrypt, ToEncrypt, 1);
-            CollectionAssert.AreEquivalent(Expect.ToArray(), OutputEncrypt.ToArray());
-            Expect.Position = 0; OutputEncrypt.Position = 0;
-            string ExpectStr = new StreamReader(Expect, encoding).ReadToEnd();
-            string OutputEncryptStr = new StreamReader(OutputEncrypt, encoding).ReadToEnd();
-            Assert.AreEqual(ExpectStr, OutputEncryptStr);
-            Assert.AreEqual(ExpectStr.Length, expect.Length);
-            Assert.AreEqual(OutputEncryptStr.Length, expect.Length);
-            Console.Out.WriteLineAsync($"{ExpectStr} : {OutputEncryptStr}");
+            string encr = GetEncryptWithTests(input, expect, encoding, key);
+            Assert.AreEqual(input, GetEncryptWithTests(encr, encoding.GetBytes(input), encoding, (byte)-key));
+        }
+
+        public string GetEncryptWithTests(string input, byte[] expect, Encoding encoding, byte key)
+        {
+            Console.WriteLine(string.Join(", ", from info in Encoding.GetEncodings() select info.Name));
+            using (MemoryStream ToEncrypt = CreateStream(input, encoding))
+                using (MemoryStream Expect = new MemoryStream(expect))
+                    using (MemoryStream OutputEncrypt = new MemoryStream())
+                    {
+
+                        Encryption.Encrypt(OutputEncrypt, ToEncrypt, key);
+
+                        Console.WriteLine("OutputEncrypt: " + string.Join(", ", OutputEncrypt.ToArray()));
+                        Console.WriteLine("Expect: " + string.Join(", ", Expect.ToArray()));
+                        CollectionAssert.AreEquivalent(Expect.ToArray(), OutputEncrypt.ToArray());
+                        Expect.Position = 0; OutputEncrypt.Position = 0;
+                        string ExpectStr = new StreamReader(Expect, encoding).ReadToEnd();
+                        string OutputEncryptStr = new StreamReader(OutputEncrypt, encoding).ReadToEnd();
+                        Assert.AreEqual(ExpectStr, OutputEncryptStr);
+                        Assert.AreEqual(ExpectStr.Length, encoding.GetString(expect).Length);
+                        Assert.AreEqual(OutputEncryptStr.Length, encoding.GetString(expect).Length);
+                        Console.Out.WriteLineAsync($"{ExpectStr} : {OutputEncryptStr}");
+                        return OutputEncryptStr;
+                    }
         }
     }
 }
