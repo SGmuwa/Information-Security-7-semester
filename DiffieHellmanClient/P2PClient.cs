53ee6e117be970cb8459371fc0a9faf6a3c0dd72
SG_Muwa <motherlode.muwa@gmail.com>
2019-10-03 16:50:20 +0300

    Упростил p2p client

--- DiffieHellmanClient/P2PClient.cs
+++ DiffieHellmanClient/P2PClient.cs
@@ -3,10 +3,8 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Timers;
-using System.IO;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using System.Linq;
 
@@ -14,20 +12,24 @@ namespace DiffieHellmanClient
 {
     class P2PClient : IDisposable
     {
-        private readonly HashSet<P2PClient> Clients = new HashSet<P2PClient>();
-        private TcpListener TcpListener;
-        /// <summary>
-        /// Используется для удалённых клиентов.
-        /// </summary>
-        private TcpClient remoteClient;
+        private readonly HashSet<TcpClient> Clients = new HashSet<TcpClient>();
+        private readonly TcpListener TcpListener;
 
-        private readonly Task taskRemover;
-        private StreamReader sr;
-        private StreamWriter sw;
         private readonly System.Timers.Timer timer = new System.Timers.Timer(20);
-        public event Action<dynamic> GetMessage;
+        /// <summary>
+        /// Происходит при получении сообщения от кого-либо.
+        /// </summary>
+        public event Action<P2PClient, dynamic> GetMessage;
+        /// <summary>
+        /// Происходит при новом подключении.
+        /// Вернуть нужно уникальный идентификатор.
+        /// </summary>
+        public event Action<P2PClient, TcpClient> OnConnection;
+        /// <summary>
+        /// Происходит при обрыве подключения.
+        /// </summary>
+        public event Action<P2PClient, TcpClient> OnDisconnection;
 
-        private readonly IPEndPoint to;
         private readonly ushort port;
 
         /// <summary>
@@ -36,57 +38,70 @@ namespace DiffieHellmanClient
         /// <param name="port">Порт, который будет прослушиваться и из которого будут идти пакеты.</param>
         public P2PClient(ushort port)
         {
+            TcpListener = new TcpListener(IPAddress.Any, port);
             TcpListener.Start();
             timer.Elapsed += TimerListner;
+            Task.Run(AcceptConnections);
             this.port = port;
-            taskRemover = Task.Run(RemoveOffline);
             timer.Start();
         }
 
-        /// <summary>
-        /// Создать виртуальный (удалённый) P2P клиент.
-        /// </summary>
-        private P2PClient(TcpClient remoteClient) { this.remoteClient = remoteClient; }
-
         public bool IsLive => timer.Enabled;
 
-        internal void AddConnection(IPEndPoint toConnect)
+        public void AddConnection(IPEndPoint toConnect)
         {
-            throw new NotImplementedException();
+            TcpClient client = new TcpClient(new IPEndPoint(IPAddress.Any, port));
+            client.Connect(toConnect);
+            Clients.Add(client);
+            OnConnection?.Invoke(this, client);
         }
 
         private void RemoveOffline()
         {
-            HashSet<P2PClient> toRemove = new HashSet<P2PClient>(from client in Clients where !client.IsLive select client);
-            foreach (P2PClient client in toRemove)
+            HashSet<TcpClient> toRemove = new HashSet<TcpClient>(from client in Clients where !client.Connected select client);
+            foreach (TcpClient client in toRemove)
             {
                 Clients.Remove(client);
+                OnDisconnection?.Invoke(this, client);
                 client.Dispose();
             }
         }
 
-        private async void AcceptConnections(TcpListener TcpListener)
+        private void AcceptConnections()
         {
-            Clients.Add(new P2PClient(await TcpListener.AcceptTcpClientAsync());
+            while (timer.Enabled)
+                if (TcpListener.Pending())
+                    Clients.Add(TcpListener.AcceptTcpClient());
+                else
+                    Thread.Sleep(50);
         }
 
 
         private void TimerListner(object sender, ElapsedEventArgs args)
         {
-
-            if (remoteClient.Available > 0)
-                GetMessage(JsonConvert.DeserializeObject(sr.ReadToEnd()));
+            RemoveOffline();
+            foreach (TcpClient client in from c in Clients where c.Available > 0 select c)
+            {
+                byte[] msg = new byte[client.Available];
+                client.GetStream().Read(msg, 0, msg.Length);
+                try
+                {
+                    GetMessage?.Invoke(this, JsonConvert.DeserializeObject(System.Text.Encoding.UTF8.GetString(msg)));
+                }
+                catch(Exception e)
+                {
+                    GetMessage?.Invoke(this, e);
+                }
+            }
         }
 
-        public void Send(dynamic toSend) => 
-            sw.Write(JsonConvert.SerializeObject(toSend));
 
         public void Dispose()
         {
-            remoteClient.Dispose();
+            TcpListener.Stop();
             timer.Dispose();
-            sr.Dispose();
-            sw.Dispose();
+            foreach (TcpClient c in Clients)
+                c.Dispose();
         }
     }
 }
